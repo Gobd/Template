666cb652bb833f68fac92c5d290cdc73c47d498f
Brian Kemper <bkemper@gmail.com>
2016-03-12 11:45:58 -0700

    done for now

---
+++ server/endpoints/endpoints.js
@@ -0,0 +1,4 @@
+var mongoose = require('mongoose');
+mongoose.Promise = require('../../bower_components/bluebird/js/browser/bluebird.min.js');
+assert.equal(query.exec().constructor, require('../../bower_components/bluebird/js/browser/bluebird.min.js'));
+var join = promise.join;
---
+++ server/index.js
@@ -0,0 +1,29 @@
+var express = require('express')
+  , bodyParser = require('body-parser')
+  , cors = require('cors')
+  , port = 8080
+  , app = express()
+  , mongoose = require('mongoose')
+  , session = require('express-session')
+  , config = require('./.config.js')
+  , db = mongoose.connection;
+
+
+mongoose.connect('mongodb://localhost/ecommerce');
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+    console.log('Connected to MongoDB!');
+});
+
+app.use(session({secret: config.secret}));
+
+app.use(express.static(__dirname + '../dist'));
+app.use(cors());
+app.options('*', cors());
+app.use(bodyParser.json());
+
+app.listen(port, function() {
+    console.log('Listening on port ' + port);
+});
+
+module.exports = app;
